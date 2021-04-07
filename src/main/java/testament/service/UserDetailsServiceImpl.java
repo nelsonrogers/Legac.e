a1e371bcc973b3096d014a68240a9043bb6f2928
BaptisteVilledieu <baptiste.villedieu2000@gmail.com>
2021-04-07 09:59:43 +0200

    Enlèvement d'erreur

--- src/main/java/testament/service/UserDetailsServiceImpl.java
+++ src/main/java/testament/service/UserDetailsServiceImpl.java
@@ -1,13 +1,13 @@
 package testament.service;
 
 import lombok.extern.slf4j.Slf4j;
-import monprojet.dao.UserRepository;
-import monprojet.entity.Utilisateur;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import testament.dao.UserRepository;
+import testament.entity.Utilisateur;
 
 @Service
 @Slf4j
