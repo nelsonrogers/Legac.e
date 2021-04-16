90606c50c472adc11ac21230b2bedbb6fd23e8ba
Nelson Rogers <nelsonlrrogers@gmail.com>
2021-04-17 00:18:42 +0200

    Scan fonctionnel, fichier de personnes décédées intégré au projet. affiche les personnes décédées sur pageAdmin après un scan.

--- src/main/java/testament/controller/ScannerController.java
+++ src/main/java/testament/controller/ScannerController.java
@@ -5,14 +5,25 @@
  */
 package testament.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import testament.dao.UserRepository;
 import testament.entity.Utilisateur;
 
@@ -20,6 +31,8 @@ import testament.entity.Utilisateur;
  *
  * @author nelsonrogers
  */
+@Controller
+@RequestMapping("/scan")
 public class ScannerController {
     
 
@@ -28,40 +41,34 @@ public class ScannerController {
     
     private ArrayList<Utilisateur> utilisateurs; 
     
+    @GetMapping("/new")
+    public String check() {
+        return "pageAdmin";
+    }
     
-    @GetMapping("/scan")
-    public ArrayList<Utilisateur> scan() {
+    
+    @PostMapping("/new")
+    public String scan(Model model) throws IOException {
         utilisateurs = new ArrayList<>();
-        
-        try {
-            File fichierDeces = new File("/Users/nelsonrogers/Desktop/deces-2021-m02.txt");
-            Scanner myReader = new Scanner(fichierDeces);
-            String data = "";
-            while (myReader.hasNextLine()) {
-                data += myReader.nextLine();
-            }
-            
+        try (FileReader fileReader = new FileReader("src/main/resources/static/deces-2021-m02.txt"); 
+            BufferedReader reader = new BufferedReader(fileReader)) {
+            String data = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+
+            System.out.println(data);
             for (Utilisateur personne : dao.findAll()) {
             
-                
-                System.out.println(personne.getNom());
-                System.out.println(personne.getPrenom());
-                System.out.println(personne.getPrenom2());
-                System.out.println(personne.getPrenom3());
-                System.out.println(personne.getSexe());
-                System.out.println(personne.getCodePostal());
-                System.out.println(personne.getCommuneNaiss());
-                
-                
+                String date = personne.getDateNaiss().toString();
+                String dateSimple = date.replace("-", "");
+                              
                 // La personne a forcément un prénom
                 String ligne = personne.getNom().toUpperCase() + "*" + personne.getPrenom().toUpperCase();
 
                 // si deuxième prénom
-                if (personne.getPrenom2() != null) {
+                if (personne.getPrenom2() != null && !personne.getPrenom2().equals("")) {
                     ligne += " " + personne.getPrenom2().toUpperCase();
                 }
                 //si troisième prénom
-                if (personne.getPrenom3() != null) {
+                if (personne.getPrenom3() != null && !personne.getPrenom3().equals("")) {
                     ligne += " " + personne.getPrenom3().toUpperCase();
                 }
 
@@ -77,8 +84,8 @@ public class ScannerController {
                     ligne += " ";
                 }
 
-
-                ligne += personne.getSexe() + personne.getDateNaiss().toString() + personne.getCodePostal() + personne.getCommuneNaiss().toUpperCase();
+                
+                ligne += personne.getSexe() + dateSimple + personne.getCodePostal() + personne.getCommuneNaiss().toUpperCase();
 
 
                 System.out.println(ligne);
@@ -86,14 +93,14 @@ public class ScannerController {
                 if(data.contains(ligne)) {
                     utilisateurs.add(personne);
                 }
-        
             }
+            model.addAttribute("utilisateurs", utilisateurs);
+            System.out.println(utilisateurs.toString());
             
-            myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        return utilisateurs;
+        return "pageAdmin";
     }
 }
